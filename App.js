dfbfcb9ef61af7aa7470785736268f4925ecd98e
Naman Jain <jainnaman1699@gmail.com>
2023-09-10 18:20:15 +0530

    Adding Info about JSX and React functional component

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+// Manually use of react createElement
 
-const heading = React.createElement("h1",
-{id : "heading"},
-"Hello World from React :)");
+// const heading = React.createElement("h1",
+// {id : "heading"},
+// "Hello World from React :)");
+
+// JSX -> JS Syntax to create React Element
+// Note : JSX is not HTML inside Javascript , It is  HTML like JS Syntax
+// JSX => React createElement => React Element(object) => HTML Element(While render)
+
+// const JSXheading = <h1 id='heading'>Hello , I am inside JSX 🚀</h1>
+let str = "I am a JSX" 
+
+// React Functional Component
+
+const A = () => <h1 id="heading">Namaste React 🚀</h1>;
+
+const B = () => {
+  return (
+    <div>
+         
+      <A />
+      {/* {A()} */}
+      {str}
+      <h1>I am inside component</h1>
+    </div>
+  );
+};
+
+// Note : when there is two component a and b and we call the component a in component b then it is known as component CompositionEvent.
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<B />);
